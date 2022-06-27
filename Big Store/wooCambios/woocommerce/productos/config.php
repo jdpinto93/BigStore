55fe90fa8140c7c35ce97af3236e07c03271f222
Jose Pinto <69821893+jdpinto93@users.noreply.github.com>
2022-06-27 18:01:57 -0400

    _cost
    Correccion en el campo de precio de costo

--- Big Store/wooCambios/woocommerce/productos/config.php
+++ Big Store/wooCambios/woocommerce/productos/config.php
@@ -15,13 +15,22 @@ return __( 'Comprar', 'woocommerce' );
 }
 
 
-//AÑADIMOS NUESTRO CAMPO PRECIO DONACIÓN
+//AÑADIMOS NUESTRO CAMPO PRECIO DE COSTO
      add_action( 'woocommerce_product_options_pricing', 'wc_cost_product_field' );
      function wc_cost_product_field() {
          woocommerce_wp_text_input( array( 'id' => '_cost', 'class' => 'wc_input_price short', 'label' => __( 'Costo', 'woocommerce' ) . ' (' . get_woocommerce_currency_symbol() . ')' ) );
      }
 
-//GUARDAMOS EL NUEVO VALOR DEL CAMPO PRECIO DONACIÓN
-     add_action( 'save_post', 'wc_cost_save_product' );
+//GUARDAMOS EL NUEVO VALOR DEL CAMPO PRECIO DE COSTO
+
      function wc_cost_save_product( $product_id ) {
-      if (wp_verify_nonce($_POST['_inline_edit'], 'inlineeditnonce'))         return;     if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )         return;     if ( isset( $_POST['_cost'] ) ) {         if ( is_numeric( $_POST['_cost'] ) )             update_post_meta( $product_id, '_cost', $_POST['_cost'] );     } else delete_post_meta( $product_id, '_cost' ); }
+      if (wp_verify_nonce($_POST['_inline_edit'], 'inlineeditnonce'));        
+          return;     
+     if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )         
+          return;     
+     if ( isset( $_POST['_cost'] ) ) {         if ( is_numeric( $_POST['_cost'] ) )             
+          update_post_meta( $product_id, '_cost', $_POST['_cost'] );     
+          } 
+     else delete_post_meta( $product_id, '_cost' ); 
+}
+     add_action( 'save_post', 'wc_cost_save_product' );
