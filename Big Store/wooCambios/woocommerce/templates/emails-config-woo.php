47b54b4714422de1375edc9e3ceb14801f2b5826
Jose Pinto <69821893+jdpinto93@users.noreply.github.com>
2022-11-22 21:00:13 -0400

    Problema de arquitectura

--- Big Store/wooCambios/woocommerce/templates/emails-config-woo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//Evita que un usuario malintencionado ejecute codigo php desde la barra del navegador
-defined('ABSPATH') or die( "Bye bye" );
-
-//Desvincula el email de nuevo pedido de los CRON --> On Hold
-add_filter( 'woocommerce_defer_transactional_emails', '__return_false' );
