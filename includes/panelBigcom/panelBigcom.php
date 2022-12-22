15c11ce992eae9eec5c0985c31552433f5d59b07
= <=>
2022-12-22 01:42:45 -0400

    Includes modules Direccion and shipping rules

--- includes/panelBigcom/panelBigcom.php
+++ includes/panelBigcom/panelBigcom.php
@@ -9,7 +9,7 @@
     <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/whatsAppLogo.png" class="img-thumbnail border-0" alt="...">
   <div class="infotarjeta">
     <h3 class="titleAdmin">Configuracion de WhatsApp</h3><br>
-    <a href="options-general.php?page=option-menu-whatsApp" class="botonTarjetas">Configurar WhatsApp</a>
+    <a href="options-general.php?page=option-menu-whatsApp" class="botonTarjetas">Ir a Configuracion</a>
   </div>
     </div>
 
@@ -17,7 +17,7 @@
     <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/googleReview.png" class="img-thumbnail border-0" alt="...">
   <div class="infotarjeta">
     <h3  class="titleAdmin">Configuración de Merchant ID</h3><br>
-    <a href="options-general.php?page=ecr_gcr" class="botonTarjetas">Configurar Merchant ID</a>
+    <a href="options-general.php?page=ecr_gcr" class="botonTarjetas">Ir a Configuracion</a>
   </div>
     </div>
 
@@ -25,7 +25,7 @@
     <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/wooCart.png" class="img-thumbnail border-0" alt="...">
   <div class="infotarjeta">
     <h3  class="titleAdmin">Configuración de Carrito</h3><br>
-    <a href="admin.php?page=side-cart-woocommerce-settings" class="botonTarjetas">Configurar Carrito</a>
+    <a href="admin.php?page=side-cart-woocommerce-settings" class="botonTarjetas">Ir a Configuracion</a>
   </div>
     </div>
   </div>
@@ -40,7 +40,7 @@
     <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/checkoutEditor.png" class="img-thumbnail border-0" alt="...">
   <div class="infotarjeta">
     <h3 class="titleAdmin">Campos Checkout</h3><br>
-    <a href="admin.php?page=th_checkout_field_editor_pro" class="botonTarjetas">Configurar Checkout</a>
+    <a href="admin.php?page=th_checkout_field_editor_pro" class="botonTarjetas">Ir a Configuracion</a>
   </div>
     </div>
 
@@ -48,7 +48,7 @@
     <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/fileUploader.png" class="img-thumbnail border-0" alt="...">
   <div class="infotarjeta">
     <h3  class="titleAdmin">Comprobante de Pago</h3><br>
-    <a href="admin.php?page=wc-settings&tab=alg_wc_checkout_files_upload" class="botonTarjetas">Configurar Boton</a>
+    <a href="admin.php?page=wc-settings&tab=alg_wc_checkout_files_upload" class="botonTarjetas">Ir a Configuracion</a>
   </div>
     </div>
 
@@ -56,7 +56,7 @@
     <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/bacTransfer.png" class="img-thumbnail border-0" alt="...">
   <div class="infotarjeta">
     <h3  class="titleAdmin">Transferencia Bancaria</h3><br>
-    <a href="admin.php?page=wc-settings&tab=checkout" class="botonTarjetas">Agregar Metodo</a>
+    <a href="admin.php?page=wc-settings&tab=checkout" class="botonTarjetas">Ir a Configuracion</a>
   </div>
     </div>
   </div>
@@ -71,7 +71,7 @@
     <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/woo]Emails.jpg" class="img-thumbnail border-0" alt="...">
   <div class="infotarjeta">
     <h3 class="titleAdmin">Configuracion de Emails</h3><br>
-    <a href="edit.php?post_type=viwec_template" class="botonTarjetas">Configurar Emails</a>
+    <a href="edit.php?post_type=viwec_template" class="botonTarjetas">Ir a Configuracion</a>
   </div>
     </div>
 
@@ -79,7 +79,7 @@
     <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/manageOrders.png" class="img-thumbnail border-0" alt="...">
   <div class="infotarjeta">
     <h3  class="titleAdmin">Configuración de Pedidos</h3><br>
-    <a href="edit.php?post_type=wc_order_status" class="botonTarjetas">Configurar Pedidos</a>
+    <a href="edit.php?post_type=wc_order_status" class="botonTarjetas">Ir a Configuracion</a>
   </div>
     </div>
 
@@ -87,7 +87,38 @@
     <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/Proyecto nuevo.png" class="img-thumbnail border-0" alt="...">
   <div class="infotarjeta">
     <h3  class="titleAdmin">Personalizar txt Botones</h3><br>
-    <a href="admin.php?page=wc-settings&tab=customizer" class="botonTarjetas">Personalizar Botones</a>
+    <a href="admin.php?page=wc-settings&tab=customizer" class="botonTarjetas">Ir a Configuracion</a>
+  </div>
+    </div>
+  </div>
+</div>
+
+<br>
+
+<div>
+  <div class="contenTarjetas">
+
+    <div class="tarjetasAdmin">
+    <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/settings.png" class="img-thumbnail border-0" alt="...">
+  <div class="infotarjeta">
+    <h3 class="titleAdmin">Ajustes Generales</h3><br>
+    <a href="options-general.php?page=bigcom-general-settings" class="botonTarjetas">Ir a Configuracion</a>
+  </div>
+    </div>
+
+    <div class="tarjetasAdmin">
+    <img src="https://s3-bigcom.s3.amazonaws.com/bigStore/multiShipping.png" class="img-thumbnail border-0" alt="...">
+  <div class="infotarjeta">
+    <h3  class="titleAdmin">Configuración de Direccines</h3><br>
+    <a href="options-general.php?page=wcmca-option-menu" class="botonTarjetas">Ir a Configuracion</a>
+  </div>
+    </div>
+
+    <div class="tarjetasAdmin">
+    <img src="" class="img-thumbnail border-0" alt="...">
+  <div class="infotarjeta">
+    <h3  class="titleAdmin"></h3><br>
+    <a href="#" class="botonTarjetas">Ir a Configuracion</a>
   </div>
     </div>
   </div>
